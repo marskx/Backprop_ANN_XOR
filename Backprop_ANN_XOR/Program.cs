df2d46ee94372dc50cc80f21206825fce43cee6d
Marsk <marskx@outlook.com>
2019-07-16 21:48:18 +0200

    Another comment was added about what check-in files that will trigger a rebuild.

--- Backprop_ANN_XOR/Program.cs
+++ Backprop_ANN_XOR/Program.cs
@@ -9,6 +9,7 @@ namespace Backprop_ANN_XOR
     /// This program is now under version control using git!
     /// It is configured to build using TeamCity
     /// A build will automatically take place on new check-ins
+    /// This will apply to all check-ins for the whole NN-Demo project.
     /// Simply provides the main entry point for the application.
     /// Creates a new frmMain form
     /// </summary>
